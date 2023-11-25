ec0b48f413037a9f326fb7146fa827706eda1522
Mohammad Hammad <hammadsmitstudent126507@gmail.com>
2023-11-26 00:29:11 +0500

    Prevent Default Practice

---
+++ app.js
@@ -0,0 +1,17 @@
+// ========== Link ==========
+
+
+// let link = document.getElementById("anchor");
+// link.addEventListener('click' , function(e) {
+//     console.log("link clicked....")
+//     e.preventDefault();
+
+// } )
+
+
+// ========== Form ==========
+
+let form = document.getElementById("my-form");
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+} )
